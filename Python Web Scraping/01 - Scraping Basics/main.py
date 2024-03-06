7ddaf5e589c94766f957e1d348d3b63bf879efb5
trevornei <117675657+trevornei@users.noreply.github.com>
2024-03-06 11:53:48 -0700

    Grabbed all h5 tags.

--- Python Web Scraping/01 - Scraping Basics/main.py
+++ Python Web Scraping/01 - Scraping Basics/main.py
@@ -5,5 +5,18 @@ from bs4 import BeautifulSoup
 # read the home.html file, name the file: 
 with open('home.html', 'r') as html_file: 
     content = html_file.read()
-    print(content)
+    
+    # create new instance of the soup library
+    soup = BeautifulSoup(content, 'lxml')
+    # print(soup.prettify())
 
+    # print the h5 tags to the terminal
+    # tags = soup.find('h5')
+    # print(tags)
+
+
+    # Print all h5 tags
+    courses_html_tags = soup.find_all('h5')
+    print(courses_html_tags)
+
+    # Grab the price for all of the courses.
